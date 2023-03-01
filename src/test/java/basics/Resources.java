95851e51cdcfc293b45489eb8d974e74bd84c95d
Gourav <gourav@g.com>
2023-03-01 11:23:42 +0530

    API automation basic project

---
+++ src/test/java/basics/Resources.java
@@ -0,0 +1,35 @@
+package basics;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class Resources {
+	
+	@Test
+	public void getListOfUsers()
+	{
+		RestAssured.baseURI = "https://reqres.in";
+		
+Response response = given()
+		
+					.when()
+		
+					.get("api/users?page=2")
+		
+					.then()
+		
+					.extract()
+		
+					.response();
+		
+	String resp = response.asPrettyString();
+
+	System.out.println(resp);
+	}
+	
+
+}
